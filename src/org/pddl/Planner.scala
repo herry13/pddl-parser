8a979a033e4ec76d95aca9bece691f88d26f183f
Herry <herry13@gmail.com>
2014-03-12 11:07:26 +0000

    update

--- src/org/pddl/Planner.scala
+++ src/org/pddl/Planner.scala
@@ -9,6 +9,7 @@ object Planner extends Planner with App {
   val sometimeBefore = "sometime-before"
   val sometimeAfter = "sometime-after"
   val atmostonce = "at-most-once"
+  val atEnd = "at-end"
   
   val help = "Usage: scala org.pddl.Planner <domain-file> <problem-file>"
   if (args.length < 2) Console.println(help)
@@ -25,16 +26,35 @@ class Planner {
   }
   
   def compileTrajectory(domain: Expression, problem: Expression) = {
+    def preprocess() = ???
+    
+    def postprocess() = ???
+    
     def compileAlways(e: Expression) =
-      println(e)
+      println(e) //TODO
     
     def compileAtMostOnce(e: Expression) = 
-      println(e)
+      println(e) //TODO
       
+    def compileSometime(e: Expression) = 
+      println(e) //TODO
+      
+    def compileSometimeAfter(e: Expression) = 
+      println(e) //TODO
+      
+    def compileSometimeBefore(e: Expression) = 
+      println(e) //TODO
+      
+    def compileAtEnd(e: Expression) =
+      println(e) //TODO
+
     def compileExpression(e: Expression) =
       if (e.label.equals(always)) compileAlways(e)
       else if (e.label.equals(atmostonce)) compileAtMostOnce(e)
-      else ???
+      else if (e.label.equals(sometime)) compileSometime(e)
+      else if (e.label.equals(sometimeAfter)) compileSometimeAfter(e)
+      else if (e.label.equals(sometimeBefore)) compileSometimeBefore(e)
+      else if (e.label.equals(atEnd)) compileAtEnd(e)
     
     ((e: Expression) =>
       if (e != null)
@@ -42,7 +62,6 @@ class Planner {
           if (item.isInstanceOf[Expression]) compileExpression(item.asInstanceOf[Expression])
           else throw new Exception("invalid constraint: " + item)
         )
-        //e.find("and").toString
       else throw new Exception("not found :constraint")
     )(problem.find(":constraints"))
     
